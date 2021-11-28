adacc32919cb2bd9cf2b325df71cd059cfdd450a
Ameen Ahmad <ameenahmad@Ameens-MacBook-Pro.local>
2021-11-28 23:37:09 +0530

    initial commit

---
+++ src/main/java/com/example/demo/myController.java
@@ -0,0 +1,19 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.lang.reflect.Method;
+
+@RestController
+public class myController {
+
+    @GetMapping(value="/")
+    public String test(){
+        return "Hello Spring";
+    }
+
+
+}
