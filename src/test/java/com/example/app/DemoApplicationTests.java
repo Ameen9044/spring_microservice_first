439b81fa669124e44c658ce78a51c2874622df9c
Ameen Ahmad <ameenahmad@Ameens-MacBook-Pro.local>
2021-12-01 00:00:43 +0530

    minor class name change

--- src/test/java/com/example/app/DemoApplicationTests.java
+++ src/test/java/com/example/app/DemoApplicationTests.java
@@ -3,7 +3,6 @@ package com.example.app;
 
 import com.example.controllers.myController;
 import org.junit.jupiter.api.Test;
-import org.mockito.internal.matchers.Equals;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
